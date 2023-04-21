f77119d5237974bbf453151f39e3c89411e1eb18
Creator Network <120567699+creatornet@users.noreply.github.com>
2023-04-21 19:26:30 +0900

    add

---
+++ Assets/Esperecyan/VRChatSRanipal/VRChatSRanipal.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using OscCore;
+using ViveSR;
+using ViveSR.anipal.Eye;
+
+public class VRChatSRanipal : MonoBehaviour
+{
+    private static readonly int VRChatReceivingPort = 9000;
+    /// <summary>
+    /// OSC Eye Tracking
+    /// https://docs.vrchat.com/docs/osc-eye-tracking
+    /// </summary>
+    private static readonly string VRChatBlinkAddress = "/tracking/eye/EyesClosedAmount";
+    private static readonly string VRChatReceivingAnimatorParameterAddressPrefix = "/avatar/parameters/";
+    private static readonly float MaxEyeGazeValue = 0.6f;
+
+    private static EyeData_v2 EyeData;
+
+    private OscClient oscClient;
+    private bool eyeDataCallbackRegisitered = false;
+    private bool needSendingDefaultEyesValue = true;
+
+    private void Start()
+    {
+        this.oscClient = new OscClient("127.0.0.1", VRChatSRanipal.VRChatReceivingPort);
+    }
+
+    private static void EyeCallback(ref EyeData_v2 eyeData)
+    {
+        VRChatSRanipal.EyeData = eyeData;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // アイトラッキングが有効
+        var eyeEnabled = SRanipal_Eye_Framework.Status == SRanipal_Eye_Framework.FrameworkStatus.WORKING;
+
+        if (eyeEnabled && !this.eyeDataCallbackRegisitered)
+        {
+            if (SRanipal_Eye_v2.WrapperRegisterEyeDataCallback(
+               Marshal.GetFunctionPointerForDelegate((SRanipal_Eye_v2.CallbackBasic)EyeCallback)
+            ) == (int)Error.NOT_INITIAL)
+            {
+                return;
+            }
+            this.eyeDataCallbackRegisitered = true;
+        }
+
+        // かつHMDをかぶっている
+        eyeEnabled = eyeEnabled && VRChatSRanipal.EyeData.no_user;
+        if (eyeEnabled || this.needSendingDefaultEyesValue)
+        {
+            var blinkLeft = 0f;
+            var blinkRight = 0f;
+            var gazeLeftX = 0.5f;
+            var gazeLeftY = 0.5f;
+            var gazeRightX = 0.5f;
+            var gazeRightY = 0.5f;
+            if (eyeEnabled && SRanipal_Eye_v2.GetEyeWeightings(out Dictionary<EyeShape_v2, float> shapes))
+            {
+                blinkLeft = shapes[EyeShape_v2.Eye_Left_Blink];
+                blinkRight = shapes[EyeShape_v2.Eye_Right_Blink];
+                gazeLeftX = Math.Clamp(shapes[EyeShape_v2.Eye_Left_Left] > 0
+                    ? 0.5f - shapes[EyeShape_v2.Eye_Left_Left] / VRChatSRanipal.MaxEyeGazeValue / 2
+                    : 0.5f + shapes[EyeShape_v2.Eye_Left_Right] / VRChatSRanipal.MaxEyeGazeValue / 2, 0, 1);
+                gazeLeftY = Math.Clamp(shapes[EyeShape_v2.Eye_Left_Down] > 0
+                    ? 0.5f - shapes[EyeShape_v2.Eye_Left_Down] / VRChatSRanipal.MaxEyeGazeValue / 2
+                    : 0.5f + shapes[EyeShape_v2.Eye_Left_Up] / VRChatSRanipal.MaxEyeGazeValue / 2, 0, 1);
+                gazeRightX = Math.Clamp(shapes[EyeShape_v2.Eye_Right_Left] > 0
+                    ? 0.5f - shapes[EyeShape_v2.Eye_Right_Left] / VRChatSRanipal.MaxEyeGazeValue / 2
+                    : 0.5f + shapes[EyeShape_v2.Eye_Right_Right] / VRChatSRanipal.MaxEyeGazeValue / 2, 0, 1);
+                gazeRightY = Math.Clamp(shapes[EyeShape_v2.Eye_Right_Down] > 0
+                    ? 0.5f - shapes[EyeShape_v2.Eye_Right_Down] / VRChatSRanipal.MaxEyeGazeValue / 2
+                    : 0.5f + shapes[EyeShape_v2.Eye_Right_Up] / VRChatSRanipal.MaxEyeGazeValue / 2, 0, 1);
+
+                this.needSendingDefaultEyesValue = true;
+            }
+            else
+            {
+                this.needSendingDefaultEyesValue = false;
+            }
+
+            if (eyeEnabled)
+            {
+                this.oscClient.Send(VRChatSRanipal.VRChatBlinkAddress, Math.Min(blinkLeft, blinkRight));
+            }
+
+            this.oscClient.Send(VRChatSRanipal.VRChatReceivingAnimatorParameterAddressPrefix + "BlinkLeft", blinkLeft);
+            this.oscClient.Send(VRChatSRanipal.VRChatReceivingAnimatorParameterAddressPrefix + "BlinkRight", blinkRight);
+            this.oscClient.Send(VRChatSRanipal.VRChatReceivingAnimatorParameterAddressPrefix + "GazeLeftX", gazeLeftX);
+            this.oscClient.Send(VRChatSRanipal.VRChatReceivingAnimatorParameterAddressPrefix + "GazeLeftY", gazeLeftY);
+            this.oscClient.Send(VRChatSRanipal.VRChatReceivingAnimatorParameterAddressPrefix + "GazeRightX", gazeRightX);
+            this.oscClient.Send(VRChatSRanipal.VRChatReceivingAnimatorParameterAddressPrefix + "GazeRightY", gazeRightY);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+VRChatSRanipal
+==============
+VIVE Pro Eye (SRanipal/SR_Runtime) のまばたきトラッキング、アイトラッキングを、OSCの9000番ポートへ以下のパラメータとして送信します。
+- /avatar/parameters/BlinkLeft
+- /avatar/parameters/BlinkRight
+- /avatar/parameters/GazeLeftX
+- /avatar/parameters/GazeLeftY
+- /avatar/parameters/GazeRightX
+- /avatar/parameters/GazeRightY
+
+[VRChatのOSC機能](https://docs.vrchat.com/docs/osc-overview) を利用してこれを受け取り、アバターへ反映することができます。
+
+VRCExpressionParametersで、上記パスの最後の部分 (「BlinkLeft」等) の名前のパラメータを設定しておくことで、パラメータ値へ反映されます。
+
+開発
+====
+クローン後、VIVE Eye and Facial Tracking SDKをインポートします。  
+https://developer-express.vive.com/resources/vive-sense/eye-and-facial-tracking-sdk/download/latest/
+
+ライセンス
+==========
+外部ライブラリを除く本リポジトリ内のソースコードはMPL-2.0です。
